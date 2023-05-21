2df6c4bf6f48f83e7118aa0e88b45f58b85efabc
NoxTGM <noxtgm@gmail.com>
2023-05-21 12:04:47 +0200

    Update sh_ras.lua

--- lua/autorun/sh_ras.lua
+++ lua/autorun/sh_ras.lua
@@ -2,28 +2,26 @@ MsgC( Color( 50, 200, 50 ), "[RAS] ", Color( 255, 255, 255 ), "Thank you for usi
 
 ---------------------------- [ ConCMD & Hooks ] ----------------------------
 
-concommand.Add( "unequipKevlar",
-	function( ply )
-		if not IsValid( ply ) or not ply:Alive() then return end -- Makes sure the player is valid and alive
-		if ( ply:GetNWBool( "wearingKevlar" ) == false ) then -- Checks if the player is not wearing a kevlar vest
-			ply:PrintMessage( HUD_PRINTCENTER, "You don't have any kevlar vest equipped" )
-		return end
+concommand.Add( "unequipKevlar", function( ply )
+	if not IsValid( ply ) or not ply:Alive() then return end -- Makes sure the player is valid and alive
+	if ( ply:GetNWBool( "wearingKevlar" ) == false ) then -- Checks if the player is not wearing a kevlar vest
+		ply:PrintMessage( HUD_PRINTCENTER, "You don't have any kevlar vest equipped" )
+	return end
 
-		ply:PrintMessage( HUD_PRINTCENTER, "You have unequipped your kevlar vest" )
+	ply:PrintMessage( HUD_PRINTCENTER, "You have unequipped your kevlar vest" )
 
-		local ent = ents.Create( "kevlar_vest" )
-		ent:SetPos( ply:GetPos() + Vector( 0, -25, 25 ) )
-		ent:Spawn()
-		ent:Activate()
-		ent:SetNWInt( "kevlarDurability", ply:Armor() ) -- Sets the entity durability to the player's armor
+	local ent = ents.Create( "kevlar_vest" )
+	ent:SetPos( ply:GetPos() + Vector( 0, -25, 25 ) )
+	ent:Spawn()
+	ent:Activate()
+	ent:SetNWInt( "kevlarDurability", ply:Armor() ) -- Sets the entity durability to the player's armor
 
-		ply:SetArmor( 0 )
-		sound.Play( "helmet_pickup.wav", Vector( ply:GetPos() ) ) -- Emit the pickup sound on the player's position
-		
-		ply:SetNWInt( "kevlarDurability", 0 )
-		ply:SetNWBool( "wearingKevlar", false )
-	end
-)
+	ply:SetArmor( 0 )
+	sound.Play( "helmet_pickup.wav", Vector( ply:GetPos() ) ) -- Emit the pickup sound on the player's position
+	
+	ply:SetNWInt( "kevlarDurability", 0 )
+	ply:SetNWBool( "wearingKevlar", false )
+end )
 
 concommand.Add( "vestDurability", function( ply )
 	if ( ply:GetNWBool( "wearingKevlar" ) == false ) then -- Checks if the player is not wearing a kevlar vest
@@ -33,8 +31,8 @@ concommand.Add( "vestDurability", function( ply )
 	ply:PrintMessage( HUD_PRINTCENTER, ply:Armor() )
 end )
 
-hook.Add( "PlayerDeath", "ras_PlayerDeath", function( victim )
-	victim:SetNWBool( "wearingVest", false )
+hook.Add( "PlayerDeath", "ras_PlayerDeath", function( ply )
+	ply:SetNWBool( "wearingVest", false )
 end )
 
 --------------------------------- [ MENU ] ---------------------------------
