63d117aa5c093eed9361b9104de0fcb339c5b694
NoxTGM <noxtgm@gmail.com>
2023-05-23 20:45:23 +0200

    Update sh_ras.lua

--- lua/autorun/sh_ras.lua
+++ lua/autorun/sh_ras.lua
@@ -1,51 +1,65 @@
 MsgC( Color( 50, 200, 50 ), "[RAS] ", Color( 255, 255, 255 ), "Thank you for using Realistic Armor System! Don't forget to like the addon on the workshop if you enjoy it <3\n" )
 
----------------------------- [ ConCMD & Hooks ] ----------------------------
+------------------------------- [ Functions ] ------------------------------
 
-concommand.Add( "unequipKevlar", function( ply )
-	if not IsValid( ply ) or not ply:Alive() then return end -- Makes sure the player is valid and alive
-	if ( ply:GetNWBool( "wearingKevlar" ) == false ) then -- Checks if the player is not wearing a kevlar vest
+local function DropArmor( ply )
+
+	if ( ply:GetNWBool( "wearingKevlar" ) == false ) then
 		ply:PrintMessage( HUD_PRINTCENTER, "You don't have any kevlar vest equipped" )
 	return end
 
 	ply:PrintMessage( HUD_PRINTCENTER, "You have unequipped your kevlar vest" )
-
+	
+	-- Create a new kevlar entity on the player's position
+	-- Store the player's armor count as the entity's durability
 	local ent = ents.Create( "kevlar_vest" )
-	ent:SetPos( ply:GetPos() + Vector( 0, -25, 25 ) )
+	ent:SetPos( ply:GetPos() + Vector( 0, 0, 25 ) )
 	ent:Spawn()
 	ent:Activate()
-	ent:SetNWInt( "kevlarDurability", ply:Armor() ) -- Sets the entity durability to the player's armor
+	ent:SetNWInt( "kevlarDurability", ply:Armor() )
 
+	-- Reset the player's armor and player stored var to 0 
+	-- Notify the player by emitting a sound on his position
+	sound.Play( "helmet_pickup.wav", Vector( ply:GetPos() ) ) 
 	ply:SetArmor( 0 )
-	sound.Play( "helmet_pickup.wav", Vector( ply:GetPos() ) ) -- Emit the pickup sound on the player's position
-	
 	ply:SetNWInt( "kevlarDurability", 0 )
 	ply:SetNWBool( "wearingKevlar", false )
-end )
+	
+end
+
+hook.Add( "PlayerDeath", "ras_PlayerDeath", DropArmor )
+
+-------------------------------- [ ConCMD ] --------------------------------
+
+concommand.Add( "unequipKevlar", DropArmor )
 
 concommand.Add( "vestDurability", function( ply )
-	if ( ply:GetNWBool( "wearingKevlar" ) == false ) then -- Checks if the player is not wearing a kevlar vest
+
+	if ( ply:GetNWBool( "wearingKevlar" ) == false ) then
 		ply:PrintMessage( HUD_PRINTCENTER, "You don't have any kevlar vest equipped" )
 	return end
 
 	ply:PrintMessage( HUD_PRINTCENTER, ply:Armor() )
-end )
 
-hook.Add( "PlayerDeath", "ras_PlayerDeath", function( ply )
-	ply:SetNWBool( "wearingVest", false )
 end )
 
 --------------------------------- [ MENU ] ---------------------------------
 
 function RASSettings( Panel )
+
 	Panel:Help( "" )
 	Panel:Button( "Unequip kevlar vest", "unequipKevlar" )
 	Panel:Button( "Check vest durability", "vestDurability" )
 	Panel:Help( "" )
+
 end
 
-function RASMenuMaker()
-	spawnmenu.AddToolMenuOption( "Options", "Realistic Armor", "rasmenu", "Settings", "custom_doit", "", RASSettings )
-end
+hook.Add( "PopulateToolMenu", "BallisticVestpleaseworkpls", function( ply )
 
-hook.Add( "PopulateToolMenu", "BallisticVestpleaseworkpls", RASMenuMaker )
+	spawnmenu.AddToolMenuOption( "Options", "Realistic Armor", "rasmenu", "Actions", "", "", RASSettings )
+
+end )
+
+--------------------------------
+--[ @NoxTGM on all platforms ]--
+--------------------------------
